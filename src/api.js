9f0ee8b656d00b080a602ec6f8f9e8663ab728bd
sunejahardik <hardiksuneja04@gmail.com>
2024-02-01 00:04:28 +0530

    final-commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '68d565b88fmsh1f593af157cc2c7p1c4230jsn29ab4c73a8b9',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '7fd684f041450d6a6b53dfa1e63370a8'
